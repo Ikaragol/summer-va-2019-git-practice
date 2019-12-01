5cd6a0ccbe24e008fcefe07cc285ccedd0fb286c
Canada54 <i_karagol@hotmail.com>
2019-11-30 19:14:52 -0800

    'asdad'

--- src/main/java/this_is_new_one.java
+++ src/main/java/this_is_new_one.java
@@ -1,7 +1,8 @@
 public class this_is_new_one {
     public static void main(String[] args) {
 
-        System.out.println("This is new one after a long time");
+        System.out.println("This is new one after a " +
+                "long time");
         System.out.println("asdasd");
     }
 }
