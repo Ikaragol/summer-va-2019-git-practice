1c99c78951fb4e2f59016b0c9a70fa5b819ce714
Canada54 <i_karagol@hotmail.com>
2019-12-19 13:35:32 -0800

    'dsa'

---
+++ src/main/java/asdasd.java
@@ -0,0 +1,10 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class asdasd {
+    public static void main(String[] args) {
+
+        System.out.println(
+                "sadsad"
+        );
+    }
+}
