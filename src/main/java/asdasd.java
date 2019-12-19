a689235443f18ea0715b76d14b63faf272fa73a3
Canada54 <i_karagol@hotmail.com>
2019-12-19 14:26:45 -0800

    'asdas'

--- src/main/java/asdasd.java
+++ src/main/java/asdasd.java
@@ -3,8 +3,7 @@ import org.w3c.dom.ls.LSOutput;
 public class asdasd {
     public static void main(String[] args) {
 
-        System.out.println(
-                "sadsaasdaasd";
+        System.out.println("sadsaasdaasd";
         );
     }
 }
